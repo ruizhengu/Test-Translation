e7ae0adeb7aac58209b7396e6ce212502e622eb7
Ruizhen Gu <ravenna1031@gmail.com>
2023-11-29 15:52:16 +0000

    update multiple approaches to implement the locators

--- Appium-Playground/Appium-Python/test.py
+++ Appium-Playground/Appium-Python/test.py
@@ -1,28 +1,45 @@
+import json
 import unittest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.android import UiAutomator2Options
+from appium.options.android import EspressoOptions
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.actions import interaction
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.actions.pointer_input import PointerInput
 
-capabilities = dict(
+# UI Automator
+capabilities_ui = dict(
     platformName='Android',
     automationName='uiautomator2',
-    # automationName='espresso',
     deviceName='emulator-5554',
-    appPackage='com.example.toyaut',
-    # appPackage='com.android.settings',
-    appActivity='.MainActivity',
-    # appActivity='.Settings',
+    # appPackage='com.example.toyaut',
+    appPackage='de.nulide.shiftcal',
+    # appActivity='.MainActivity',
+    appActivity='.CalendarActivity',
+    language='en',
+    locale='US'
+)
+
+# Espresso
+capabilities_es = dict(
+    platformName='Android',
+    automationName='espresso',
+    deviceName='emulator-5554',
+    app='/Users/ruizhengu/Projects/Test-Translation/ShiftCal-solution/app/build/outputs/apk/debug/app-debug.apk',
+    appPackage='de.nulide.shiftcal',
+    appActivity='.CalendarActivity',
     language='en',
     locale='US'
 )
 
 appium_server_url = 'http://localhost:4723'
 
-capabilities = UiAutomator2Options().load_capabilities(capabilities)
+capabilities = UiAutomator2Options().load_capabilities(capabilities_ui)
+
+
+# capabilities = EspressoOptions().load_capabilities(capabilities_es)
 
 
 class TestAppium(unittest.TestCase):
@@ -35,8 +52,9 @@ class TestAppium(unittest.TestCase):
 
     def test_find_battery(self) -> None:
         # el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Hello Android!"]')
-        el = self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="text(\"Hello Android!\")")
-        # el = self.driver.find_element(by=AppiumBy.ANDROID_VIEW_MATCHER, value="text(\"Hello Android!\")")
+        # el = self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="text(\"Hello Android!\")")
+        el = self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="resourceId(\"de.nulide.shiftcal:id/btnPopup\")")
+        # el = self.driver.find_element("id", "de.nulide.shiftcal:id/btnPopup")
         el.click()
         # self.long_click(el)
 
